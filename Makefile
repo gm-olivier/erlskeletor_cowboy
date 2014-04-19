69f19bb823cf5468b11bd35a41cc148f0ca0a6cf
pyotrgalois <federico.carrone@gmail.com>
2014-04-19 05:34:06 +0000

    added base mixin

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 PROJECT = erlskeletor
 
-DEPS = cowboy lager jiffy ibrowse eper
+DEPS = cowboy lager jiffy ibrowse eper mixer
 dep_cowboy = https://github.com/extend/cowboy.git master
 dep_lager = https://github.com/basho/lager.git master
 dep_jiffy = https://github.com/davisp/jiffy.git master
 dep_ibrowse= https://github.com/cmullaparthi/ibrowse.git
 dep_eper = https://github.com/massemanet/eper.git
+dep_mixer = https://github.com/opscode/mixer.git
 
 include erlang.mk
 
--- src/erlskeletor_root_handler.erl
+++ src/erlskeletor_root_handler.erl
@@ -1,34 +1,20 @@
-%% @doc Handler for / endpoints
 -module(erlskeletor_root_handler).
+-include_lib("mixer/include/mixer.hrl").
+-mixin([{erlskeletor_base,
+         [
+          init/3,
+          allowed_methods/2,
+          content_types_provided/2,
+          is_authorized/2
+         ]
+        }]).
 
 -export(
-  [
-   init/3,
-   allowed_methods/2,
-   content_types_provided/2,
-   is_authorized/2
-  ]).
-
--export(
-  [
+   [
     handle_get/2
-  ]).
+   ]
+).
 
-%% cowboy
-init(_Transport, _Req, _Opts) ->
-    {upgrade, protocol, cowboy_rest}.
-
-allowed_methods(Req, State) ->
-    {[<<"GET">>], Req, State}.
-
-content_types_provided(Req, State) ->
-    {[{{<<"application">>, <<"json">>, []}, handle_get}], Req, State}.
-
-is_authorized(Req, State) ->
-    {true, Req, State}.
-
-%% internal
 handle_get(Req, State) ->
-    Body = jiffy:encode({[{<<"status">>, <<"ok">>}]}),
-    lager:info("holas"),
+    Body = jiffy:encode({[{<<"foo">>, <<"foo">>}]}),
     {Body, Req, State}.
