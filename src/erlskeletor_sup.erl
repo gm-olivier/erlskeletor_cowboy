2790769a75cc7aeb521afbf42f2f64fd3d109037
Federico Carrone <federico.carrone@gmail.com>
2014-06-02 12:29:07 -0300

    updated style

--- src/erlskeletor_sup.erl
+++ src/erlskeletor_sup.erl
@@ -1,48 +1,54 @@
 -module(erlskeletor_sup).
--author('federico.carrone@inakanetworks.net').
+-author('federico.carrone@gmail.com').
 
 -behaviour(supervisor).
 
--export([start_link/0, start_listeners/0]).
--export([init/1]).
+-export([
+         start_link/0,
+         start_listeners/0
+        ]).
+-export([
+         init/1
+        ]).
 
 %% admin api
-start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, {}).
+start_link() -> 
+    supervisor:start_link({local, ?MODULE}, ?MODULE, {}).
 
 start_listeners() ->
-  {ok, Port} = application:get_env(erlskeletor, http_port),
-  {ok, ListenerCount} = application:get_env(erlskeletor, http_listener_count),
-
-  Dispatch =
-    cowboy_router:compile([
-                           {
-                             '_',
-                             [
-                              {<<"/">>, erlskeletor_root_handler, []}
-                             ]
-                           }
-                          ]),
+    {ok, Port} = application:get_env(erlskeletor, http_port),
+    {ok, ListenerCount} = application:get_env(erlskeletor, http_listener_count),
     
-  RanchOptions =
-    [ 
-      {port, Port}
-    ],
-  CowboyOptions =
-    [ 
-      {env, [
-             {dispatch, Dispatch}
-            ]},
-      {compress,  true},
-      {timeout,   12000}
-    ],
-
-  cowboy:start_http(erlskeletor_http, ListenerCount, RanchOptions, CowboyOptions).
+    Dispatch =
+        cowboy_router:compile([
+                               {
+                                 '_',
+                                 [
+                                  {<<"/">>, erlskeletor_root_handler, []}
+                                 ]
+                               }
+                              ]),
+    
+    RanchOptions =
+        [ 
+          {port, Port}
+        ],
+    CowboyOptions =
+        [ 
+          {env, [
+                 {dispatch, Dispatch}
+                ]},
+          {compress,  true},
+          {timeout,   12000}
+        ],
+    
+    cowboy:start_http(erlskeletor_http, ListenerCount, RanchOptions, CowboyOptions).
 
 %% behaviour callbacks
 init({}) ->
-  {ok, { {one_for_one, 5, 10},
-    [ 
-      %{ChildId, StartFunc, Restart, Shutdown, Type, Modules}.
-      {erlskeletor_http, {erlskeletor_sup, start_listeners, []}, permanent, 1000, worker, [erlskeletor_sup]}
-    ]}
-  }.
+    {ok, { {one_for_one, 5, 10},
+           [ 
+             %{ChildId, StartFunc, Restart, Shutdown, Type, Modules}
+             {erlskeletor_http, {erlskeletor_sup, start_listeners, []}, permanent, 1000, worker, [erlskeletor_sup]}
+           ]}
+    }.
